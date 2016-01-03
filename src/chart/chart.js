cfd5665b88f38f495fbe8a43b6aa1f188f1bd00d
Nik <nik@congral.com>
2016-01-02 21:38:52 -0500

    remove unused functions

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -1,10 +1,10 @@
 import {customElement, bindable, inject} from 'aurelia-framework';
 import {WidgetBase} from '../common/widget-base';
 import {generateBindables} from '../common/decorators';
-import {PDF} from '../pdf/pdf'; //eslint-disable-line no-unused-vars
+//import {PDF} from '../pdf/pdf'; //eslint-disable-line no-unused-vars
 import 'kendo-ui/js/kendo.dataviz.chart.min';
-import 'kendo-ui/js/kendo.dataviz.chart.polar.min';
-import 'kendo-ui/js/kendo.dataviz.chart.funnel.min';
+//import 'kendo-ui/js/kendo.dataviz.chart.polar.min';
+//import 'kendo-ui/js/kendo.dataviz.chart.funnel.min';
 
 @customElement('k-chart')
 @generateBindables('kendoChart')
@@ -22,23 +22,23 @@ export class Chart extends WidgetBase {
     this._initialize();
   }
 
-  exportImage(options) {
-    if (this.widget) {
-      return this.widget.exportImage(options);
-    }
-  }
+  // exportImage(options) {
+  //   if (this.widget) {
+  //     return this.widget.exportImage(options);
+  //   }
+  // }
 
-  exportPDF(options) {
-    if (this.widget) {
-      return this.widget.exportPDF(options);
-    }
-  }
+  // exportPDF(options) {
+  //   if (this.widget) {
+  //     return this.widget.exportPDF(options);
+  //   }
+  // }
 
-  exportSVG(options) {
-    if (this.widget) {
-      return this.widget.exportSVG(options);
-    }
-  }
+  // exportSVG(options) {
+  //   if (this.widget) {
+  //     return this.widget.exportSVG(options);
+  //   }
+  // }
 
   getAxis(name) {
     if (this.widget) {
@@ -64,11 +64,11 @@ export class Chart extends WidgetBase {
     }
   }
 
-  saveAsPDF() {
-    if (this.widget) {
-      return this.widget.saveAsPDF();
-    }
-  }
+  // saveAsPDF() {
+  //   if (this.widget) {
+  //     return this.widget.saveAsPDF();
+  //   }
+  // }
 
   setDataSource(dataSource) {
     if (this.widget) {
@@ -82,11 +82,11 @@ export class Chart extends WidgetBase {
     }
   }
 
-  svg() {
-    if (this.widget) {
-      return this.widget.svg();
-    }
-  }
+  // svg() {
+  //   if (this.widget) {
+  //     return this.widget.svg();
+  //   }
+  // }
 
   imageDataURL() {
     if (this.widget) {
